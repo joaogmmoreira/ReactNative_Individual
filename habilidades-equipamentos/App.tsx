c85fb5eca4fa6fdecb87354d60239360fdc21cde
João Moreira <jgmoreira1809@gmail.com>
2024-11-10 21:28:44 -0300

    Update App.tsx

--- habilidades-equipamentos/App.tsx
+++ habilidades-equipamentos/App.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 // import { Skills } from "./src/screens/skills/index";
-import { Player } from "./src/screens/spotifyPlayer/index";
-// import { Playlist } from "./src/screens/spotifyPlaylist/index";
+// import { Player } from "./src/screens/spotifyPlayer/index";
+import { Playlist } from "./src/screens/spotifyPlaylist/index";
 
 export default function App() {
-  // return <Spotify />;
-  return <Player />;
+  return <Playlist />;
+  // return <Player />;
 }
